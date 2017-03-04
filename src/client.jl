53df40436c4c4960c0f519c411eec51a64391cf1
Elliot Saba <staticfloat@gmail.com>
2017-03-04 15:10:50 -0800

    Backup

--- src/client.jl
+++ src/client.jl
@@ -1,15 +1,21 @@
 module Client
 using HTTP
 
-client = HTTP.Client()
+# Explicitly use a client whose chunksize is well above our chunk size so that
+# we don't accidentally trigger chunked encoding
+client = HTTP.Client(chunksize=2*1024*1024)
 res = nothing
+
+function upload_chunk(file::IOStream, target::AbstractString,
+                      chunk_idx::Integer, chunk_size::Integer)
+
 
 function upload_file(file_path::AbstractString, target::AbstractString;
                      chunk_size::Integer = 1*1024*1024,
                      mime_type::MIME = MIME(""))
     global client, res
 
-    filename = basename(file_path)[1:end-4]
+    filename = basename(file_path)
     total_size = filesize(file_path)
     num_chunks = ceil(Int,total_size/chunk_size)
 
@@ -30,15 +36,15 @@ function upload_file(file_path::AbstractString, target::AbstractString;
 
         # Check each chunk before uploading the full chunk
         uri = HTTP.URI(target; query=params)
-        res = HTTP.get(client, uri; verbose=true)
+        res = HTTP.get(client, uri)
         if res.status != 200
             # That chunk is not there yet, let's post it up.
-            seek(f, chunk_idx*chunk_size - 1)
+            seek(f, (chunk_idx - 1)*chunk_size)
             data = read(f, chunk_size)
             params["file"] = IOBuffer(data)
 
             println("Uploading chunk $chunk_idx...")
-            res = HTTP.post(client, uri; body=params, verbose=true)
+            res = HTTP.post(client, uri; body=params)
             if res.status != 200
                 error("Upload failed with HTTP code $(res.status)")
             end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,18 +3,25 @@ using Base.Test
 
 # First, kick off the docker container that contains the reference Node
 # Resumable.js implementation, so we can interact with it.
-container_id = readchomp(`docker run -p 3000:3000/tcp -d staticfloat/resumeablejs_test_server`)
-println("Test server running in container $(container_id[1:10])")
+container_id = readchomp(`docker run --rm -p 3000:3000/tcp -d staticfloat/resumeablejs_test_server`)
+info("Test server running in container $(container_id[1:10])")
 
-# Generate a 5MB file that will cause us to upload 5 chunks
+# Generate a 5MB+ file that will cause us to upload 5 (and a little more) chunks
 bigfile = open("bigfile", "w")
-write(bigfile, rand(5*1024*1024))
+write(bigfile, rand(UInt8, 5*1024*1024 + 1337))
 
 try
     # Upload the big file to the Node.JS server
     Resumable.Client.upload_file("bigfile", "http://localhost:3000/upload")
 
+    # Do it again to ensure that we don't need to upload those pieces again
+    Resumable.Client.upload_file("bigfile", "http://localhost:3000/upload")
+
     # TODO verify the big file uploaded properly
 finally
+    info("Showing logs for docker test container:")
+    run(`docker logs $container_id`)
+
+    info("Killing docker server container:")
     success(`docker kill $container_id`)
 end
